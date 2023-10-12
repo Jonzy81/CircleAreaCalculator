c95411bf7d5c029768ae63d053b92b46f4926bb2
jonnytouma <jonnytouma@gmail.com>
2023-10-12 11:57:40 +0200

    Add project files.

---
+++ CircleAreaCalculator/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CircleAreaCalculator
+{
+    internal class Circle
+    {
+        int _radius;
+        public Circle (int radius)
+        {
+            _radius = radius;
+        }
+        public double GetRadius(int radius)
+        {
+            double areaCircle= Math.PI*Math.Pow(radius,2);
+            double areaSphere = 4 * Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"Arean för cirkeln med radien {radius}cm är {areaCircle:0.00} cm i kvadrat, " +
+                $"\nifall du undrar så är arean av cirkeln ifall den vore en sfär {areaSphere:0.00} cm i kvadrat\n");
+            return areaCircle;
+        }
+    }
+}
---
+++ CircleAreaCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CircleAreaCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skapa en ny klass som heter Circle
+            //Skapa en konstruktor som tar in radien 
+            //Skapa en metod som heter GetCircle och som räknar ut arean på en cirkel 
+            //Räkan ut arean på en cirkel med radien 5 och en med radien 6 
+
+            Circle firstCircle = new Circle(5);
+            Circle secondCircle = new Circle(6);
+
+            firstCircle.GetRadius(5);
+            secondCircle.GetRadius(6);
+
+
+        }
+    }
+}
